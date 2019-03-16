428492d44608ee12a2703dfb13559c936272f0b2
yoann roche <el-rambo-poto@hotmail.fr>
2019-03-16 11:26:35 +0100

    Ajout du diagramme de classe de la base de données. Ajout des requetes SQL INSERT des tables note et reservation, il manque seulement la table image à remplir

---
+++ SQL/INSERT/note.sql
@@ -0,0 +1,7 @@
+INSERT INTO note ('note', `site_id`,'topo_id','compte_id') VALUES
+(7,2,null,1),
+(9,1,null,2),
+(7,2,null,1),
+(8,null,1,3),
+(9,null,2,1),
+(6,null,2,2);
---
+++ SQL/INSERT/reservation.sql
@@ -0,0 +1,4 @@
+INSERT INTO reservation ('date','matin','apres_midi','compte_id','topo_id') VALUES
+('16/03/2019',TRUE,FALSE,1,1),
+('23/03/2019',TRUE,TRUE,2,1),
+('07/04/2019',FALSE,TRUE,2,3);
